73dcfc0e22432268a741ee9ed371a14b8b80a15f
GGeraGG <gennadiy.chickunov@gmail.comm>
2021-12-13 15:33:44 +0200

    worked with NavLink + .active

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,22 @@
 import classes from './Sidebar.module.scss';
+import { NavLink } from "react-router-dom";
 //console.log(classes);
 const Sidebar = () => {
     return (
-            <nav className={classes.nav}>
-                <ul>
-                    <li className={`${classes.item} ${classes.active}`}>
-                        <a href="/profile">Profile</a>
-                    </li>
-                    <li className={classes.item}>
-                        <a href="/dialogs">Messages</a>
-                    </li>
-                    <li className={classes.item}>SAAS</li>
-                    <li className={classes.item}>HTML5</li>
-                </ul>
-            </nav>
+           <aside>
+               <nav className={classes.nav}>
+                   <ul>
+                       <li className={classes.item}>
+                           <NavLink to='/profile'>Profile</NavLink>
+                       </li>
+                       <li className={classes.item}>
+                           <NavLink to='/dialogs'>Dialogs</NavLink>
+                       </li>
+                       <li className={classes.item}>SAAS</li>
+                       <li className={classes.item}>HTML5</li>
+                   </ul>
+               </nav>
+           </aside>
         );
 }
 
