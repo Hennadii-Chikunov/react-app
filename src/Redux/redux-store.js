4d3bf20f4d6f94ddada6fefc4feee8504f7d2391
GGeraGG <gennadiy.chickunov@gmail.comm>
2022-01-24 16:08:35 +0200

    Refactor... and Learn cookie, login in theory, auth/me

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./Reducers/profile-reducer";
 import dialogsReducer from "./Reducers/dialogs-reducer";
 import sidebarReducer from "./Reducers/sidebar-reducer";
 import usersReducer from './Reducers/users-reducer';
+import authReducer from "./Reducers/auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebarPage: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import logo from '../../img/logo.jpg';
 import styles from './Header.module.scss';
+import {NavLink} from "react-router-dom";
 
-const Header = () => (
-       <header className={styles.header}>
-           <img className={styles.logo} src={logo} alt={"logo"}/>
-       </header>
-    );
+const Header = (props) => (
+    <header className={styles.header}>
+        <img className={styles.logo} src={logo} alt={"logo"}/>
+        <div className={styles.login}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
+    </header>
+);
 
-export { Header };
+export {Header};
