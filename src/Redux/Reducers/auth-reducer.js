84a697c6b9e005f64436f5c0e1799426d904219b
GGeraGG <gennadiy.chickunov@gmail.comm>
2022-02-02 15:38:52 +0200

    Refactor...changed npm on the yarn, add + learn Redux-Thunk

--- src/Redux/Reducers/auth-reducer.js
+++ src/Redux/Reducers/auth-reducer.js
@@ -1,3 +1,5 @@
+import {instance, usersAPI} from "../../api/api";
+
 const SET_USER_DATA = 'SET_USER_DATA';
 
 let initialState = {
@@ -21,6 +23,17 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
+export const auth = () => {
+    return (dispatch) => {
+        usersAPI.authMe()
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    let {id, login, email} = response.data.data;
+                    dispatch(setAuthUserData(id, email, login));
+                }
+            });
+    }
+}
 
 export const setAuthUserData = (UserId, email, login) => ({type: SET_USER_DATA, data: {UserId, email, login}})
 export default authReducer;
--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import profileReducer from "./Reducers/profile-reducer";
 import dialogsReducer from "./Reducers/dialogs-reducer";
 import sidebarReducer from "./Reducers/sidebar-reducer";
 import usersReducer from './Reducers/users-reducer';
 import authReducer from "./Reducers/auth-reducer";
+import thunkMiddleware from 'redux-thunk';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -13,7 +14,7 @@ let reducers = combineReducers({
     auth: authReducer
 });
 
-let store = createStore(reducers);
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
